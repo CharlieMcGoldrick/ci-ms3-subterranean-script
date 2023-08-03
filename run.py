06fc62d2969815ecadd3e1449415ea895f3ff0e0
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-08-03 01:28:01 +0000

    Feat: add start screen with ASCII title

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Write your code to expect a terminal of 80 characters wide and 24 rows high   g
+print("            __       _     _                                           ") 
+print("           / _\_   _| |__ | |_ ___ _ __ _ __ ___  __ _ _ __            ")  
+print("           \ \| | | | '_ \| __/ _ \ '__| '__/ _ \/ _` | '_ \           ")
+print("           _\ \ |_| | |_) | ||  __/ |  | | |  __/ (_| | | | |          ")
+print("           \__/\__,_|_.__/ \__\___|_|  |_|  \___|\__,_|_| |_|          ")
+print("                       __           _       _                          ") 
+print("                      / _\ ___ _ __(_)_ __ | |_                        ")  
+print("                      \ \ / __| '__| | '_ \| __|                       ")
+print("                      _\ \ (__| |  | | |_) | |_                        ")
+print("                      \__/\___|_|  |_| .__/ \__|                       ")
+print("                                     |_|                               ")   
+print("                                                                       ")   
+print("             Welcome to the depths of 'Subterranean Script'!           ")
+print("           This is a text-based, choice-driven adventure game.         ")
+print("             Navigate through the all-encompassing darkness            ")
+print("            where every door opens a new path, a new destiny.          ")
+print("                                                                       ")
+print("         Note: Whisper 'help' anytime to conjure the command list      ")
+print("                                                                       ")
+print("         Ready to step into the unknown? Type 'Enter' if you dare      ")
+
+
