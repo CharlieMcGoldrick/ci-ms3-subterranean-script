2f1f9febe924ae3a9a772fe4e7efd116cc97e6a5
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-08-10 14:45:42 +0000

    Chore: add docstrings to all functions

--- run.py
+++ run.py
@@ -13,22 +13,49 @@ character = {
 def print_help(current_state, previous_state):
     """
     Prints a help message describing the available commands based on the
-    current state.
+    current state and previous state of the game.
+
+    This function provides contextual guidance to the player, offering
+    different commands and information depending on the current state of
+    the game. The help message may include basic commands like
+    'Return' and 'Exit', and specific guidance based on the player's current
+    location or situation within the game.
+
+    :param current_state: The current state of the game, used to determine
+           what specific guidance should be provided.
+    :param previous_state: The previous state of the game, also used to
+           provide context-specific help.
     """
     print("------------------------------------------------------")
     print("\nYou whispered for help... The shadows respond:")
     print("'Return': Resume your previous action.")
     print("'Exit' : Wake from the dream and return to reality.")
     if previous_state == game_states.STATE_NAME:
-        print("\nType your name to continue, a name is more than just an identity here...")
+        print("\nType your name to continue, a name is more than just an"
+              "identity here...")
     # ... More states will go here!
 
 
 def handle_universal_commands(user_input, current_state, previous_state):
+    """
+    Handles universal commands that are available in multiple states of the
+    game.
+
+    :param user_input: The input provided by the user.
+    :param current_state: The current state of the game.
+    :param previous_state: The previous state of the game.
+
+    :return: The next game state if a universal command is recognized and
+             handled, or None if the user input does not correspond to a
+             universal command. The 'stats' command is only recognized if
+             both the name and stats attributes of the character are
+             initialized.
+    """
     if user_input == 'help':
         print_help(current_state, previous_state)
         return game_states.STATE_HELP
-    elif user_input == 'stats' and character['name'] is not None and character['stats'] is not None:
+    elif (user_input == 'stats' and character['name']
+          is not None and character['stats'] is not None):
         print_stats()
         return previous_state
     return None
@@ -36,7 +63,14 @@ def handle_universal_commands(user_input, current_state, previous_state):
 
 def print_stats():
     """
-    Prints the current character's stats.
+    Prints the current character's stats in a formatted manner.
+
+    This function retrieves the character's name and stats from the global
+    'character' dictionary, and prints them line by line, aligning the
+    stat names to the left with a specific width for a clean presentation.
+
+    :note: Assumes that the 'character' dictionary contains 'name' and
+           'stats' keys with valid values.
     """
     print(f"{character['name']}, your current stats are:")
     for stat, value in character["stats"].items():
@@ -45,7 +79,14 @@ def print_stats():
 
 def roll_stats():
     """
-    Rolls stats for a new character.
+    Rolls stats for a new character using the 4d6 drop lowest method.
+
+    This function rolls four 6-sided dice for each attribute, removes the
+    lowest roll, and sums the remaining three to generate the value for
+    each attribute. The attributes being rolled for are: Strength,
+    Dexterity, Constitution, Intelligence, Wisdom, and Charisma.
+
+    :return: A dictionary containing the rolled values for each attribute.
     """
     attributes = ['Strength', 'Dexterity', 'Constitution', 'Intelligence',
                   'Wisdom', 'Charisma']
@@ -60,6 +101,19 @@ def roll_stats():
 
 
 def handle_start_state(user_input):
+    """
+    Handles the starting state of the game where the player is prompted to
+    enter the game.
+
+    :param user_input: The input provided by the user. It must be the string
+                       'enter'.
+
+    :return: The next game state, which is the state for entering the
+             character's name, if the user types 'enter'.
+
+    :raises ValueError: If the user input is not 'enter', an error message
+                        is raised to prompt the player to type 'Enter'.
+    """
     if user_input != 'enter':
         raise ValueError("\nThe shadows were quite explicit. Type 'Enter'.")
     print("------------------------------------------------------")
@@ -74,8 +128,24 @@ def handle_start_state(user_input):
 
 
 def handle_name_state(user_input):
+    """
+    Handles the state where the player is prompted to enter their character's
+    name.
+
+    :param user_input: The input provided by the user. It must be a valid name
+                       consisting of alphabetic characters, not exceeding 20
+                       characters.
+
+    :return: The next game state, which is the state following the entry of,
+              the name if the user provides a valid name.
+
+    :raises ValueError: If the user input contains numbers or symbols, is
+                        empty, or exceeds 20 characters, an error message
+                        is raised to guide the player to input a valid name.
+    """
     if user_input == 'help':
-        print("Type your name to continue. A name is more than just an identity here.")
+        print("Type your name to continue. A name is more than just an"
+              "identity here.")
         print("'Help' : Seek guidance from the shadows.")
         print("'Exit' : Wake from the dream and return to reality.")
         return game_states.STATE_NAME  # Stay in the same state
@@ -99,28 +169,42 @@ def handle_name_state(user_input):
     return game_states.STATE_NEXT
 
 
-# def handle_stats_state():
-#     character["stats"] = roll_stats()
-#     print("------------------------------------------------------")
-#     print("As you navigate the darkness, your competency comes flooding"
-#           " back...")
-#     for stat, value in character["stats"].items():
-#         print(f"Your {stat:<15} is {value}")
-#     # Placeholder
-#     return game_states.STATE_NEXT
-
-
 def main_game_loop():
+    """
+    Manages the main gameplay loop, orchestrating the progression between
+    different game states.
+
+    This function maintains a loop that keeps the game running, managing
+    transitions between different states according to user inputs and
+    current game conditions. It prompts the player for inputs based
+    on the current state and manages how the game responds.
+
+    The game states handled by this loop include:
+        - Help state (providing help and guidance)
+        - Stats state (displaying character's statistics)
+        - Start state (initial entry into the game)
+        - Name state (naming the character)
+
+    Exceptions such as ValueError will be caught and handled, allowing for
+    the game loop to continue.
+
+    :note: The user can type 'return' in specific states to go back to a
+           previous state.
+    :note: The function utilizes helper functions like handle_start_state,
+           handle_name_state, etc., to manage specific states.
+    """
     current_state = game_states.STATE_START
     # Store the previous state to return to
     previous_state = None
     while True:
         if current_state == game_states.STATE_HELP:
-            prompt = "What do you demand of the shadows? Type 'return' to go back."
+            prompt = "What do you demand of the shadows? Type 'return' to go"
+            "back."
         elif current_state == game_states.STATE_STATS:
             prompt = "If you've finished looking at yourself, type 'return'"
         elif current_state == game_states.STATE_START:
-            prompt = "Ready to step into the unknown? Type 'Enter' if you dare."
+            prompt = "Ready to step into the unknown? Type 'Enter' if you"
+            "dare."
         elif current_state == game_states.STATE_NAME:
             prompt = "What does it say on your arm?"
         else:
@@ -130,15 +214,18 @@ def main_game_loop():
             user_input = input(f"\n{prompt}\n").lower()
 
             # Check for universal commands
-            new_state = handle_universal_commands(user_input, current_state, previous_state)
+            new_state = handle_universal_commands(user_input, current_state,
+                                                  previous_state)
             if new_state is not None:
                 if current_state != new_state:
                     previous_state = current_state
                 current_state = new_state
                 continue
 
-            if user_input == 'return' and current_state in (game_states.STATE_HELP, game_states.STATE_STATS):
-                current_state = previous_state or game_states.STATE_NAME  # return to the previous state or a default state
+            if (user_input == 'return' and current_state in
+               (game_states.STATE_HELP, game_states.STATE_STATS)):
+                # return to the previous state or a default state
+                current_state = previous_state or game_states.STATE_NAME
                 continue
 
             if current_state == game_states.STATE_START:
@@ -154,6 +241,17 @@ def main_game_loop():
 
 
 def start_game():
+    """
+    Initiates the text-based adventure game 'Subterranean Script'.
+
+    This function displays the game's title, a brief introduction, and a
+    welcome message to guide players into the mysterious and all-encompassing
+    darkness of the game world.
+
+    It then calls the main_game_loop() function to start the gameplay.
+
+    :note: Whisper 'help' anytime in the game to view a list of commands.
+    """
     print(" __       _     _                                                 ")
     print("/ _\\_   _| |__ | |_ ___ _ __ _ __ ___  __ _ _ __                 ")
     print("\\ \\| | | | '_ \\| __/ _ \\ '__| '__/ _ \\/ _` | '_ \\           ")
